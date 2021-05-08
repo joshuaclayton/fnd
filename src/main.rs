bcce5b7fc463837a22674ff4d2d5c3d64c5e3a73
Joshua Clayton <joshua.clayton@gmail.com>
2021-05-08 14:16:02 -0400

    Allow dot

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use ignore::{WalkBuilder, WalkState};
 use regex::Regex;
+use std::path::Path;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Flags {
-    query: String,
+    query: Option<String>,
 
     #[structopt(short, long)]
     all: bool,
@@ -17,30 +18,57 @@ struct Flags {
     regex: bool,
 }
 
-fn main() {
-    let opts = Flags::from_args();
+enum PathCheck<'a> {
+    Allow,
+    CheckRegex(Regex),
+    CheckStr(&'a str),
+}
 
+impl<'a> PathCheck<'a> {
+    fn check(&self, path: &Path) -> bool {
+        match self {
+            PathCheck::Allow => true,
+            PathCheck::CheckStr(query) => path.to_string_lossy().contains(query),
+            PathCheck::CheckRegex(regex) => regex.is_match(&path.to_string_lossy()),
+        }
+    }
+
+    fn new(flags: &'a Flags) -> Self {
+        if let Some(ref query) = flags.query {
+            if flags.regex {
+                if let Some(re) = Regex::new(&query).ok() {
+                    PathCheck::CheckRegex(re)
+                } else {
+                    PathCheck::CheckStr(&query)
+                }
+            } else {
+                if query == "." {
+                    PathCheck::Allow
+                } else {
+                    PathCheck::CheckStr(&query)
+                }
+            }
+        } else {
+            PathCheck::Allow
+        }
+    }
+}
+
+fn main() {
+    let flags = Flags::from_args();
     let mut builder = WalkBuilder::new("./");
-    if opts.all {
+
+    if flags.all {
         builder.git_ignore(false);
     }
 
-    let mut re = None;
-    if opts.regex {
-        re = Regex::new(&opts.query).ok();
-    }
+    let check = PathCheck::new(&flags);
 
     builder.threads(20).build_parallel().run(|| {
         Box::new(|result| {
             if let Some(path) = result.ok().map(|entry| entry.path().to_path_buf()) {
-                if let Some(regex) = &re {
-                    if regex.is_match(&path.to_string_lossy()) {
-                        println!("{}", path.to_string_lossy());
-                    }
-                } else {
-                    if path.to_string_lossy().contains(&opts.query) {
-                        println!("{}", path.to_string_lossy());
-                    }
+                if check.check(&path) {
+                    println!("{}", path.to_string_lossy());
                 }
             }
 
